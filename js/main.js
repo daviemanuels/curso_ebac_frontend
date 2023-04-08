afdbd375cfc96b2c30b7a552e745bbfdec8f48f2
Davi Emanuel <daviiemanuel2000@gmail.com>
2023-04-08 14:48:22 -0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+    $('#telefone').mask('(00) 00000-0000', {
+        placeholder: '(__) _____-____'
+    })
+
+    $('#cpf').mask('000.000.000-00', {
+        placeholder: 'Apenas números'
+    })
+    $('#cep').mask('00000-000')
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            cpf: {
+                required: true
+            },
+            enderecoCompleto: {
+                required: true
+            },
+            cep: {
+                required: true
+            }
+        },
+        messages: {
+            nome: 'Por favor, insira o seu nome completo',
+        },
+        submitHandler: function(form) {
+            console.log(form)
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if (camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos incorretos`)
+            }
+        }
+    })
+
+})
