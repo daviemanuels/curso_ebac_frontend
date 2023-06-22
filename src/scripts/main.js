6b6a8c012329f4bef735e1d538ff16894e9035e2
Davi Emanuel <daviiemanuel2000@gmail.com>
2023-06-22 16:31:59 -0300

    Update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,19 @@
+// let button = document.getElementById('send');
+
+// button.addEventListener('click', function() {
+
+//     let showMessage = document.getElementById('aparecer');
+
+//     if(showMessage.style.display === "none") {
+//         showMessage.style.display = "block"
+//     } else {
+//         showMessage.style.display = "none"
+//     }
+// })
+
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('send').addEventListener('click', function() {
-        let showMessage = document.getElementById('aparecer');
 
-        if(showMessage.style.display === "none") {
-            showMessage.style.display = "block"
-        } else {
-            showMessage.style.display = "none"
-        }
+        document.querySelector('#aparecer').style.display = 'block';
     })
 })
