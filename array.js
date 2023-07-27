24ffcc75517529501764d4087863d44dfb1cdeed
Davi Emanuel <daviiemanuel2000@gmail.com>
2023-07-27 10:04:15 -0300

    update alunosAprovados

---
+++ array.js
@@ -0,0 +1,17 @@
+const arrayAlunos = [
+    { aluno: 'Davi', nota: 9.0},
+    { aluno: 'Melissa', nota: 9.5},
+    { aluno: 'Emanuel', nota: 7.0},
+    { aluno: 'Gabriel', nota: 5.5},
+    { aluno: 'Aluízio', nota: 4.3},
+    { aluno: 'William', nota: 3.5},
+    { aluno: 'Gabiela', nota: 8.7},
+    { aluno: 'Hiuri', nota: 6.0},
+    { aluno: 'José', nota: 5.9},
+];
+
+const alunosAprovados = arrayAlunos.filter((aluno) => {
+    return aluno.nota >= 6;
+})
+
+console.log(alunosAprovados)
