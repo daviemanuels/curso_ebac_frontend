a3e1bd314bb44652cde0f95e116146e762671dd3
Davi Emanuel <daviiemanuel2000@gmail.com>
2023-07-27 10:05:22 -0300

    second update

--- array.js
+++ array.js
@@ -7,11 +7,11 @@ const arrayAlunos = [
     { aluno: 'William', nota: 3.5},
     { aluno: 'Gabiela', nota: 8.7},
     { aluno: 'Hiuri', nota: 6.0},
-    { aluno: 'José', nota: 5.9},
+    { aluno: 'José', nota: 5.9}
 ];
 
 const alunosAprovados = arrayAlunos.filter((aluno) => {
     return aluno.nota >= 6;
 })
 
-console.log(alunosAprovados)
+console.log(alunosAprovados);
